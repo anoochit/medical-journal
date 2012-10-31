82ad3f2a4a3d3df812e3ce4daffe349119396c01
Anuchit Chalothorn <anoochit@gmail.com>
2012-10-31 17:35:03 +0700

    update core for non debug mode

--- mjapi/app/Controller/UserController.php
+++ mjapi/app/Controller/UserController.php
@@ -1,34 +1,34 @@
 <?php
 class UserController extends AppController {
- 
+
 	public function index() {
 		//$this->set('results', $this->User->find('all'));
-    }
-    
-    function viewalluser() {
-    	$this->layout = 'ajax';
-    	$users = $this->User->find('all', array('recursive' => -1));
-    	$this->set(compact('results'));
-    }
-    
-    function authen() {
-    	$this->layout = 'ajax';
-    	
-    	$data=$this->params['named'];
-    	$username=$data['u'];
-    	$password=$data['p'];
-    	
-    	if (($username=="admin") AND ($password=="admin")) {
-    		$req="0";
-    		$msg="ok";
-    	} else {
-    		$req="1";
-    		$msg="not authen";
-    	}
-    	
-    	$results=array('req'=>$req,'msg'=>$msg);    	
-    	$this->set(compact('results'));
-    }
-    
- 
+	}
+
+	function viewalluser() {
+		$this->layout = 'ajax';
+		$users = $this->User->find('all', array('recursive' => -1));
+		$this->set(compact('results'));
+	}
+
+	function authen() {
+		$this->layout = 'ajax';
+		 
+		$data=$this->params['named'];
+		$username=$data['u'];
+		$password=$data['p'];
+		 
+		if (($username=="admin") AND ($password=="admin")) {
+			$req="0";
+			$msg="ok";
+		} else {
+			$req="1";
+			$msg="not authen";
+		}
+		 
+		$results=array('req'=>$req,'msg'=>$msg);
+		$this->set(compact('results'));
+	}
+
+
 }
