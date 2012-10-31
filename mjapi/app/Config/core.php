a2a06ebfd1726f12acfb73416795bcc487f2905b
Anuchit Chalothorn <anoochit@gmail.com>
2012-10-31 17:06:12 +0700

    add json view for user controller

--- mjapi/app/Config/core.php
+++ mjapi/app/Config/core.php
@@ -30,7 +30,7 @@
  * 	2: As in 1, but also with full debug messages and SQL output.
  *
  * In production mode, flash messages redirect after a time interval.
- * In development mode, you need to click the flash message to continue.
+ * In development mode, you need to click the flash message to continue. * 
  */
 	Configure::write('debug', 2);
 
--- mjapi/app/Config/routes.php
+++ mjapi/app/Config/routes.php
@@ -26,6 +26,10 @@
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
 	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+//	Router::connect('/', array('controller' => 'services',  ));
+
+	// json extension
+	Router::parseExtensions('json');
 /**
  * ...and connect the rest of 'Pages' controller's urls.
  */
